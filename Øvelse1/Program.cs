ef0351cd312f7fa643450776f861984aec69d05b
Real-vokS <danielcskov@gmail.com>
2022-04-29 10:26:35 +0200

    Add project files.

---
+++ Øvelse1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Øvelse1
+{
+    class Program
+    {
+
+        static Thread t1 = new Thread(SayEasy);
+        static void Main(string[] args)
+        {
+            t1.Name = "Thread1";
+            t1.Start();
+            Console.Read();
+        }
+
+        static void SayEasy()
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("[{0}] C# trådning er nemt", Thread.CurrentThread.Name);
+        }
+
+    }
+}
